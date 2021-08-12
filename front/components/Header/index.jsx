3054c32a556fb7ffa039d1021535311155ff2f92
momo <wdudtlrw78@gmail.com>
2021-08-12 16:42:24 +0900

    Add: header-mobile

--- front/components/Header/index.jsx
+++ front/components/Header/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import MobileNav from '../MobileNav';
+import './styles.scss';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav>
+        <div className="left__group">
+          <MobileNav />
+          <button type="button" className="search-btn">
+            <i className="fas fa-search" />
+          </button>
+        </div>
+        <a href="/" className="logo">
+          MOMO
+        </a>
+        <a href="/user/cart" className="cart-btn">
+          <span>CART(0)</span>
+        </a>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
--- front/layouts/App/index.jsx
+++ front/layouts/App/index.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import BodyContainer from '@components/BodyContainer';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
+import CartPage from '../../pages/CartPage';
 
 const HomeScreen = loadable(() => import('@pages/HomeScreen'));
 
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomeScreen} />
+        <Route exact path="/user/cart" component={CartPage} />
       </Switch>
       <Footer />
     </BodyContainer>
