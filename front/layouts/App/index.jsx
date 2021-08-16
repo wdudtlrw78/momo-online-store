5d0d15955b3fa832d06f3fe2db5b829323b767bf
momo <wdudtlrw78@gmail.com>
2021-08-17 01:31:32 +0900

    Add: login, register page layouts

--- front/layouts/App/index.jsx
+++ front/layouts/App/index.jsx
@@ -4,10 +4,11 @@ import { Switch, Route } from 'react-router';
 import BodyContainer from '@components/BodyContainer';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-import Cart from '@pages/Cart';
-import Login from '@pages/Login';
 
 const Home = loadable(() => import('@pages/Home'));
+const Login = loadable(() => import('@pages/Login'));
+const Register = loadable(() => import('@pages/Register'));
+const Cart = loadable(() => import('@pages/Cart'));
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
         <Route exact path="/user/cart" component={Cart} />
       </Switch>
       <Footer />
--- front/pages/Register/index.jsx
+++ front/pages/Register/index.jsx
@@ -1,13 +1,13 @@
 import React, { useCallback, useEffect, useRef } from 'react';
 import './styles.scss';
 
-function Login() {
-  const onEmailFocus = useRef(null);
+function Register() {
+  const onNicknameFocus = useRef(null);
 
   const onSubmit = useCallback(() => {}, []);
 
   useEffect(() => {
-    onEmailFocus.current.focus();
+    onNicknameFocus.current.focus();
   }, []);
 
   return (
@@ -18,22 +18,30 @@ function Login() {
       </div>
       <div className="login-form">
         <form onSubmit={onSubmit}>
+          <label className="Nickname-label">
+            <span>Nickname</span>
+            <input type="Nickname" ref={onNicknameFocus} />
+          </label>
           <label className="email-label">
             <span>Email</span>
-            <input type="email" ref={onEmailFocus} />
+            <input type="email" />
           </label>
           <label className="password-label">
             <span>Password</span>
             <input type="password" />
           </label>
-          <button type="submit">LOG IN</button>
+          <label className="confirm-password-label">
+            <span>Password</span>
+            <input type="password" />
+          </label>
+          <button type="submit">REGISTER</button>
         </form>
         <p className="link-container">
-          New Customer? <a href="/register">REGISTER</a>
+          Have an Account? <a href="/login">LOGIN</a>
         </p>
       </div>
     </div>
   );
 }
 
-export default Login;
+export default Register;
