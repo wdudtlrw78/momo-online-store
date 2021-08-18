db98036c610595f1458d2713ad9b668ca32cc769
momo <wdudtlrw78@gmail.com>
2021-08-19 01:54:06 +0900

    Fix: loader component layout

--- front/components/Loader/index.jsx
+++ front/components/Loader/index.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import loadingGif from '../../public/Loading.gif';
 
 function Loader() {
-  return <img src={loadingGif} className="loading" alt="Loading" />;
+  return (
+    <div style={{ width: '100%', maxWidth: '100rem', padding: '0 1rem', margin: '0 auto' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <img src={loadingGif} className="loading" alt="Loading" />
+      </div>
+    </div>
+  );
 }
 
 export default Loader;
