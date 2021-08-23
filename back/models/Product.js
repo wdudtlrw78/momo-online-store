bca2c6193253b2d386f47344f22a4fe97ee40383
momo <wdudtlrw78@gmail.com>
2021-08-23 22:52:54 +0900

    Add: product and review models

---
+++ back/models/Product.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const reviewSchema = mongoose.Schema(
+  {
+    nickname: {
+      type: String,
+      required: true,
+    },
+
+    rating: {
+      type: Number,
+      required: true,
+    },
+
+    comment: {
+      type: String,
+      required: true,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+
+    gender: {
+      type: String,
+      require: true,
+    },
+
+    menProductCategory: {
+      type: Number,
+    },
+
+    womenProductCategory: {
+      type: Number,
+    },
+
+    title: {
+      type: String,
+      maxlength: 50,
+      require: true,
+    },
+
+    description: {
+      type: String,
+      require: true,
+    },
+
+    reviews: [reviewSchema],
+
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      default: 0,
+      require: true,
+    },
+
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    images: {
+      type: Array,
+      default: [],
+      require: true,
+    },
+
+    sold: {
+      type: Number,
+      maxlength: 100,
+      defalut: 0,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = { Product };
