13edc665db90ac366bfa5b96549fc9e0df36a205
momo <wdudtlrw78@gmail.com>
2021-09-13 22:30:01 +0900

    config.js url

--- front/config/config.js
+++ front/config/config.js
@@ -1,3 +1,3 @@
-export const USER_SERVER = process.env.NODE_ENV === 'production' ? 'https://api.momostore.com' : '/api/users';
-export const PRODUCT_SERVER = process.env.NODE_ENV === 'production' ? 'https://api.momostore.com' : '/api/product';
-export const SERVER_URL = process.env.NODE_ENV === 'production' ? 'https://api.momostore.com' : 'http://localhost:3410';
+export const USER_SERVER = process.env.NODE_ENV === 'production' ? 'http://13.125.146.87' : '/api/users';
+export const PRODUCT_SERVER = process.env.NODE_ENV === 'production' ? 'http://13.125.146.87' : '/api/product';
+export const SERVER_URL = process.env.NODE_ENV === 'production' ? 'http://13.125.146.87' : 'http://localhost:3410';
