5f706b1d2ce2e740b21c9b8d1cf162422ac3efd2
momo <wdudtlrw78@gmail.com>
2021-09-16 01:00:23 +0900

    Fix: re name

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './front/dist')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, './front', 'index.html'))
+    res.sendFile(path.join(__dirname, './front/dist', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => {
