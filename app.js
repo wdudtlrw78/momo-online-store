332f57899efa15d9c5e0b7b03f2febdd4b431ce0
momo <wdudtlrw78@gmail.com>
2021-09-16 02:22:41 +0900

    Add: package.json homepage

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'front/dist')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'front/dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'front', 'dist', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => {
